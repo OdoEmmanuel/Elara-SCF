31dd6a28ea9a21114977cd944e27fbcc9f4a4ef2
akinbo-emmanuel <akinbo.emmanuel@lmu.edu.ng>
2025-10-01 12:33:40 +0100

    refactor: simplify homepage layout and add primary color variable

---
+++ components/auth/layout/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="py-5 px-12 flex">
+      <div className="border border-primary py-2.5 px-6 rounded-sm">
+        <p className="font-semibold text-sm/[150%] text-primary">LOGO</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
